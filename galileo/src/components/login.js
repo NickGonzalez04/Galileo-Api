060e188335a5b4bb70061d640f843caa906339b5
Nicholas Gonzalez <gonzaleznick5@gmail.com>
2020-08-31 10:08:10 -0400

    01_react_app_&_login
    - Created React app
    - Created Login in compontent

---
+++ galileo/src/components/login.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+
+const Login = ()=> {
+
+
+    return (
+        <div>
+            <form>
+                <header>Ether</header>
+                <h3>User Login</h3>
+                <input type="text" value="Username" placeholder="Username"></input>
+                <input type="password" value="Password" placeholder="Password"></input>
+                <hr></hr>
+
+            </form>
+        </div>
+    )
+};
+
+export default Login;
