a31853eaf6e5997d2eabf88ebca070ddd66d568e
Nicholas Gonzalez <gonzaleznick5@gmail.com>
2020-09-01 15:45:15 -0400

    03_useEffect_Hook

--- galileo/src/components/LendingDash.js
+++ galileo/src/components/LendingDash.js
@@ -4,19 +4,22 @@ import axios from 'axios'
 
 const LendingDash = ()=> {
     
-    const onSubmit=() => {
+ useEffect(() => {
+    
        axios.post('https://sandbox.galileo-ft.com/instant/v1/login',{ username:"RLfPLRtyD8cK",
        password:"Y2AQ0hEh307UzG6RxWLa" })
         .then(response => {
             console.log(response);
         }).catch(err => { console.log(err) })
-    }
+    });
+
+
     return (
         <div>
             <header>Ether Lending</header>
             <body>
                 <div>
-                    <button onClick={onSubmit}>Lending</button>
+                    <button>Lending</button>
                 </div>
             </body>
         </div>
