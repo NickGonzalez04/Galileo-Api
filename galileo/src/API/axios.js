7ba38a87cf2be0750d00733950a018ae3f961d7a
Nicholas Gonzalez <gonzaleznick5@gmail.com>
2020-09-03 22:16:59 -0400

    06_cookie_header

--- galileo/src/API/axios.js
+++ galileo/src/API/axios.js
@@ -2,9 +2,10 @@ import axios from 'axios';
 
  const axiosWithAuth = () => {
     return axios.create({
-        baseURL: 'https://sandbox.galileo-ft.com/instant/v1/',
+        baseURL: 'https://sandbox.galileo-ft.com/instant/v1',
         headers:{
-            'Access-Control-Allow-Origin': '*',
+            // 'Cookie':'f5avraaaaaaaaaaaaaaaa_session_=CMGPGGGHFBDHHKCCHMOIDGBHOEAKEGBKFEDIFHLDOEFFPINMKHDCAEHOHJHLJMELLJODBFOHLHBBJIPOPLIAIOLGHBEDNDFIEDJJKENJICOHLLMNJEPJIHHAAPLKLAHG; TS012d9947=012fd6b128e3f8ef7758972c1cb20ccb8cf95899b5a9c083662d0a743b4819afa96399b8b7f53fa99aa221127090362a36e4dc2ec2',
+            'Cache-Control':'no-cache',
             'Content-type': 'application/json', 
         }
     })
--- galileo/src/App.js
+++ galileo/src/App.js
@@ -10,12 +10,20 @@ import './App.css';
 function App() {
 
   useEffect(()=> {
-    axios.post('httpS://sandbox.galileo-ft.com/instant/v1/login',{ username:"RLfPLRtyD8cK",
-    password:"Y2AQ0hEh307UzG6RxWLa" })
+    console.log('console log')
+    axios.post('https://sandbox.galileo-ft.com/instant/v1/login',{ 
+    username:"RLfPLRtyD8cK",
+    password:"Y2AQ0hEh307UzG6RxWLa"
+  },{ headers:{
+    'Content-Type': 'application/json', 
+    'Cookie': 'f5avraaaaaaaaaaaaaaaa_session_=CMGPGGGHFBDHHKCCHMOIDGBHOEAKEGBKFEDIFHLDOEFFPINMKHDCAEHOHJHLJMELLJODBFOHLHBBJIPOPLIAIOLGHBEDNDFIEDJJKENJICOHLLMNJEPJIHHAAPLKLAHG; TS012d9947=012fd6b128e3f8ef7758972c1cb20ccb8cf95899b5a9c083662d0a743b4819afa96399b8b7f53fa99aa221127090362a36e4dc2ec2'
+}})
      .then(response => {
          console.log('here', response);
-     }).catch(err => { console.log(err) })
- });
+     }).catch(error => { console.log('error',error) })
+ },[]);
+ 
+
   return (
     <div className="App">
       <header className="App-header">
